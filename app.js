4104d0cb923b8e1b7f77470fe3e8690f4930ba2a
ysw <15926462114@163.com>
2018-12-14 20:54:13 +0800

    注册信息更新完毕

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express();
-
+const bodyParser = require('body-parser')
+//引入mysql模块：
+const mysql=require("mysql");
+//引入moment时间就算模版：
+const moment=require("moment");
+//连接数据裤
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'blog'
+  })
+  
+app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(express.static('./views'))
 // 虚拟目录
 app.use('/node_modules', express.static('./node_modules')) 
@@ -11,10 +24,48 @@ app.set('view engine', 'ejs')
 // 设置模板页面的存放路径  
 app.set('views', './views') 
 
-//注：访问不能有出现./"
+//注：访问不能有出现./
+//   进去首页
 app.get("/",(req,res) =>{
     res.render("index.ejs")  //这个地方可以点写index.ejs或者index 会自动获取
 })
+//  进入注册页
+app.get("/register",(req,res) => {
+    res.render("./user/register.ejs")
+})
+//  进入登录页
+app.get('/login', (req, res) => {
+    res.render('./user/login', {})
+  })
+
+//  注册用户信息
+app.post("/register",(req,res) => {
+    let userInfo=req.body;
+    if (!userInfo.username || !userInfo.nickname || !userInfo.password) return res.status(400).send({ status: 400, msg: '请输入正确的表单信息!' })
+
+    const chachongSql = 'select count(*) as count from users where username = ?'
+    conn.query(chachongSql,userInfo.username,(err,result) =>{
+        if(err) return res.status(500).send({ status: 500, msg: '查重失败!请重试!' })
+        // result[0].count // 为0表示没有重复, 可以用
+    if (result[0].count !== 0) return res.status(400).send({ status: 400, msg: '用户名重复!请重试!' })
+    // 能到此处 说明可以注册
+    // 添加ctime字段
+    userInfo.ctime = moment().format('YYYY-MM-DD HH:mm:ss')
+    // console.log(userInfo)
+    // 执行注册的sql语句  使用MySQL模块 注意insert into xxx set
+    const registerSql = 'insert into users set ?'
+    conn.query(registerSql, userInfo, (err, result) => {
+      if (err) return res.status(500).send({ status: 500, msg: '注册失败!请重试!' })
+      // 让浏览器跳到登录页
+      // 302重定向
+      // res.redirect('/login')
+      res.send({ status: 200, msg: '注册成功!' })
+    })
+    })
+})
+
+
+
 
 app.listen(3000,function(){
     console.log("http://192.168.14.21:3000")
