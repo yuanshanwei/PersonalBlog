d56fcb9dbe27e0cbce8bd9b416ea2ea18b6cfcf7
ysw <15926462114@163.com>
2018-12-14 21:07:08 +0800

    登录页面更新

--- app.js
+++ app.js
@@ -42,28 +42,28 @@ app.get('/login', (req, res) => {
 app.post("/register",(req,res) => {
     let userInfo=req.body;
     if (!userInfo.username || !userInfo.nickname || !userInfo.password) return res.status(400).send({ status: 400, msg: '请输入正确的表单信息!' })
-
     const chachongSql = 'select count(*) as count from users where username = ?'
     conn.query(chachongSql,userInfo.username,(err,result) =>{
         if(err) return res.status(500).send({ status: 500, msg: '查重失败!请重试!' })
-        // result[0].count // 为0表示没有重复, 可以用
     if (result[0].count !== 0) return res.status(400).send({ status: 400, msg: '用户名重复!请重试!' })
-    // 能到此处 说明可以注册
-    // 添加ctime字段
     userInfo.ctime = moment().format('YYYY-MM-DD HH:mm:ss')
-    // console.log(userInfo)
-    // 执行注册的sql语句  使用MySQL模块 注意insert into xxx set
     const registerSql = 'insert into users set ?'
     conn.query(registerSql, userInfo, (err, result) => {
       if (err) return res.status(500).send({ status: 500, msg: '注册失败!请重试!' })
-      // 让浏览器跳到登录页
-      // 302重定向
-      // res.redirect('/login')
       res.send({ status: 200, msg: '注册成功!' })
     })
     })
 })
 
+//用户登录
+app.post('/login', (req, res) => {
+    let loginInfo=req.body;
+    const loginSql = 'select * from users where username = ? and password = ?'
+    conn.query(loginSql, [loginInfo.username, loginInfo.password], (err, result) => {
+      if (err || result.length === 0) return res.status(400).send({ status: 400, msg: '登录失败!请重试!' })
+      res.send({ status: 200, msg: '登录成功!' })
+    })
+  })
 
 
 
