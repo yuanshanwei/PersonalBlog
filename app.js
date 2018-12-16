d61be5bb8c98b5c6e86656962b1180ab0fd2c08d
ysw <15926462114@163.com>
2018-12-16 21:03:11 +0800

    session设置完毕！

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+//导入session模版
+const session=require("express-session");
+// 注册一个session中间件;
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    // 如果不设置过期时间  默认 关闭浏览器即过期, 无法存储有效的cookie
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 }  
+  }))
+
+
 //引入mysql模块：
 const mysql=require("mysql");
 //引入moment时间就算模版：
@@ -28,7 +40,10 @@ app.set('views', './views')
 //   进去首页
 //注：http访问不能有出现./"
 app.get("/",(req,res) =>{
-    res.render("index.ejs")  //这个地方可以点写index.ejs或者index 会自动获取
+    res.render("index.ejs",{
+        userInfo: req.session.userInfo, //在页面当中获取session的值
+        isLogin: req.session.isLogin    //在页面当中获取session的值
+      })  //这个地方可以点写index.ejs或者index 会自动获取
 })
 //  进入注册页
 app.get("/register",(req,res) => {
@@ -36,7 +51,8 @@ app.get("/register",(req,res) => {
 })
 //  进入登录页
 app.get('/login', (req, res) => {
-    res.render('./user/login', {})
+    res.render('./user/login', {   
+    })
   })
 
 //  注册用户信息
@@ -62,11 +78,24 @@ app.post('/login', (req, res) => {
     const loginSql = 'select * from users where username = ? and password = ?'
     conn.query(loginSql, [loginInfo.username, loginInfo.password], (err, result) => {
       if (err || result.length === 0) return res.status(400).send({ status: 400, msg: '登录失败!请重试!' })
+      //登录成功我们将用户信息保存在session中：
+      // 将用户信息存储在session中    
+      req.session.userInfo = result[0]
+      // 记录登录状态
+      req.session.isLogin = true
+
       res.send({ status: 200, msg: '登录成功!' })
+
     })
   })
-
-
+ 
+  //注销
+  app.get("/logout",(req,res) => {
+    req.session.destroy((err) => {
+        // res.send({status: 200, msg: '退出登录成功!'})
+        res.redirect('/')  //重定向到首页
+    })
+  })
 
 
 
